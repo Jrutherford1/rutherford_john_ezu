613f7476bf50dbb404494217bf7332ced73243fa
John Rutherford <johnrr3@illinois.edu>
2023-06-23 14:46:59 -0500

    absolute url updates for list pages

--- rutherford_john_ezu/urls.py
+++ rutherford_john_ezu/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import redirect_root_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("courseinfo.urls"))
+    path("", include("courseinfo.urls")),
+    path("", redirect_root_view),
 ]
 
